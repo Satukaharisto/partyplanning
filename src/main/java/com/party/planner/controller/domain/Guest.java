9c87c3ee331ab3d678576cdf4648b769c0f9988d
Sandra <sandra_wikman@hotmail.com>
2018-04-24 23:27:14 +0200

    working guestlist

--- src/main/java/com/party/planner/controller/domain/Guest.java
+++ src/main/java/com/party/planner/controller/domain/Guest.java
@@ -1,43 +1,48 @@
 package com.party.planner.controller.domain;
 
-public class Guests {
+public class Guest {
 
-//    private int id;
+    private int id;
     private String firstname;
     private String lastname;
     private String gender;
 
-    public Guests(int id, String firstname, String lastname, String gender) {
-//        this.id = id;
+    public Guest(int id, String firstname, String lastname, String gender) {
+        this.id = id;
         this.firstname = firstname;
         this.lastname = lastname;
         this.gender = gender;
     }
 
-//    public int getId() {
-//        return id;
-//    }
-//    public void setId(int id) {
-//        this.id = id;
-//    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getFirstname() {
         return firstname;
     }
+
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
+
     public String getLastname() {
         return lastname;
     }
+
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
+
     public String getGender() {
         return gender;
     }
+
     public void setGender(String gender) {
         this.gender = gender;
     }
-
 }
--- src/main/java/com/party/planner/controller/repository/PartyRepository.java
+++ src/main/java/com/party/planner/controller/repository/PartyRepository.java
@@ -1,13 +1,17 @@
 package com.party.planner.controller.repository;
 
+import com.party.planner.controller.domain.Guest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class PartyRepository implements Repository {
+
     @Autowired
     private DataSource dataSource;
 
@@ -28,24 +32,32 @@ public class PartyRepository implements Repository {
             }
             return userId;
         } catch (SQLException e) {
-            throw new RepositoryExceptions("Nu blev det supertokigt i när du skapar en user", e);
-        }
-    }
-    @Override                   //GÄSTER    ------------- här kopplar vi ihop databasen med HTML filen
-    public void addGuest(String firstname, String lastname, String gender) {
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement ps = conn.prepareStatement("INSERT INTO [dbo].[Guests]([FirstName],[LastName],[Gender]) " +
-                     "VALUES (?,?,?) ")) {
-            ps.setString(1, firstname);
-            ps.setString(2, lastname);
-            ps.setString(3, gender);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw new RepositoryExceptions("Nu blev det supertokigt i PartyRepo", e);
+            throw new RepositoryExceptions("Nu blev det supertokigt i addUser - partyrepo", e);
         }
     }
 
-    public boolean checkLogin(String username, String password) {
+    @Override                   //GÄSTER    ------------- här kopplar vi ihop databasen med HTML filen
+    public int addGuest(String firstname, String lastname, String gender, int userId) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement("INSERT INTO [dbo].[Guests]([FirstName],[LastName],[Gender], [User_ID]) " +
+                     "VALUES (?,?,?,?) ", Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, firstname);
+            ps.setString(2, lastname);
+            ps.setString(3, gender);
+            ps.setInt(4, userId);
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            int guestId = -1;
+            while (rs.next()) {
+                guestId = rs.getInt(1);
+            }
+            return guestId;
+        } catch (SQLException e) {
+            throw new RepositoryExceptions("Nu blev det supertokigt i addguest - partyrepo", e);
+        }
+    }
+
+    public Integer checkLogin(String username, String password) {
         try (Connection conn = dataSource.getConnection();
 
              PreparedStatement ps = conn.prepareStatement("SELECT [UserID] FROM  [dbo].[Users] WHERE ([UserName] = (?) AND [Password] = (?)) ")) {
@@ -53,12 +65,33 @@ public class PartyRepository implements Repository {
             ps.setString(2, password);
             ResultSet results = ps.executeQuery();
             if (results.next()) {
-                return true;
+                return results.getInt("UserId");
             }
-            return false;
+            return null;
         } catch (SQLException e) {
-            throw new RepositoryExceptions("Nu blev det supertokigt i PartyRepo", e);
+            throw new RepositoryExceptions("Nu blev det supertokigt i checklogin - PartyRepo", e);
         }
     }
 
+    @Override
+    public List<Guest> getGuestList(int userId) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement("SELECT GuestId, FirstName, lastname, gender from Guests\n " +
+                     "WHERE User_ID = (?) ")) {
+            ps.setInt(1, userId);
+
+            ResultSet rs = ps.executeQuery();
+            List<Guest> guests = new ArrayList<>();
+            while (rs.next()) {
+                guests.add(new Guest(rs.getInt("GuestId"),
+                        rs.getString("FirstName"),
+                        rs.getString("lastname"),
+                        rs.getString("gender")));
+
+            }
+            return guests;
+        } catch (SQLException e) {
+            throw new RepositoryExceptions("Nu blev det supertokigt i checklogin - PartyRepo", e);
+        }
+    }
 }
