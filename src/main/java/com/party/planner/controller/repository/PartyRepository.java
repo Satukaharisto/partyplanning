4e5c79072eb725867661357115b38e00faada363
Sofia Axelson <axelson_sofia@hotmail.com>
2018-05-02 17:25:22 +0200

    add encrypted pasword function

--- src/main/java/com/party/planner/controller/repository/PartyRepository.java
+++ src/main/java/com/party/planner/controller/repository/PartyRepository.java
@@ -2,6 +2,7 @@ package com.party.planner.controller.repository;
 
 
 import com.party.planner.controller.domain.*;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -23,17 +24,38 @@ public class PartyRepository implements Repository {
              PreparedStatement ps = conn.prepareStatement("INSERT INTO [dbo].[User3]([UserName], [Password], [Email]) " +
                      "VALUES (?,?,?) ", Statement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, userName);
-            ps.setString(2, password);
+            ps.setString(2, BCrypt.hashpw(password, BCrypt.gensalt())); //krypterar lösenordet i databasen
             ps.setString(3, email);
             ps.executeUpdate();
             ResultSet rs = ps.getGeneratedKeys();
             int userId = -1;
             while (rs.next()) {
                 userId = rs.getInt(1);
+
             }
+
             return userId;
         } catch (SQLException e) {
             throw new RepositoryExceptions("something went wrong in adduser - PartyRepository", e);
+        }
+    }
+
+    @Override
+    public Integer checkLogin(String username, String password) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement("SELECT [UserID] , [Password] FROM [dbo].[User3] WHERE ([UserName] = (?) ) ")) {
+            ps.setString(1, username);
+            ResultSet results = ps.executeQuery();
+
+            if (results.next()) {
+                String hashedPassword = results.getString("Password");
+                if (BCrypt.checkpw(password, hashedPassword)) {
+                    return results.getInt("UserId");
+                }
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new RepositoryExceptions("something went wrong in checklogin - PartyRepository", e);
         }
     }
 
@@ -154,22 +176,6 @@ public class PartyRepository implements Repository {
 
 // LOGIN
 
-    @Override
-    public Integer checkLogin(String username, String password) {
-        try (Connection conn = dataSource.getConnection();
-
-             PreparedStatement ps = conn.prepareStatement("SELECT [UserID] FROM [dbo].[User3] WHERE ([UserName] = (?) AND [Password] = (?)) ")) {
-            ps.setString(1, username);
-            ps.setString(2, password);
-            ResultSet results = ps.executeQuery();
-            if (results.next()) {
-                return results.getInt("UserId");
-            }
-            return null;
-        } catch (SQLException e) {
-            throw new RepositoryExceptions("something went wrong in checklogin - PartyRepository", e);
-        }
-    }
 
     @Override
     public void updateGuest(int eventId, int id, String firstname, String lastname, String email, String gender) {
@@ -416,6 +422,7 @@ public class PartyRepository implements Repository {
             throw new RepositoryExceptions("something went wrong in addevent - PartyRepository", e);
         }
     }
+
     @Override
     public List<Event> getEventList(int userId) {
         try (Connection conn = dataSource.getConnection();
