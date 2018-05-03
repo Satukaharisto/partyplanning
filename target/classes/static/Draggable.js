e6a31ee231e2eb7f5e134f06d0e6a258639e97ed
Madeleine Karlsson <madeleine.karlsson@outlook.com>
2018-05-03 12:02:02 +0200

    Added the seatingarrangement

---
+++ target/classes/static/Draggable.js
@@ -0,0 +1,41 @@
+
+let dragMoveListener = window.dragMoveListener = function (e) {
+    let target = e.target,
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + e.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + e.dy;
+
+    target.style.zIndex = 10; // bring dragged element to front
+
+    target.style.webkitTransform =
+        target.style.transform =
+            `translate(${x}px, ${y}px)`;
+
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+};
+
+let cleanNode = function (node) {
+    node.removeAttribute('data-x');
+    node.removeAttribute('data-y');
+    node.removeAttribute('style');
+    return node;
+};
+
+let handleEnd = function (e) {
+    if (e.target.classList.contains('blueprint')) {
+        let nodeCopy = cleanNode(e.target.cloneNode(true)); // copy node and remove attributes
+        e.target.parentNode.insertBefore(nodeCopy, e.target);
+        e.target.classList.remove('blueprint');
+    }
+
+    e.target.style.zIndex = 1; // reset bring to front
+};
+
+interact('.draggable').draggable({
+    restrict: {
+        restriction: "body" // let object be dropped within the entire body
+    },
+    onmove: dragMoveListener,
+    onend: handleEnd
+});
+  
